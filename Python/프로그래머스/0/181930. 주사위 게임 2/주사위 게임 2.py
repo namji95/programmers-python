b9ef5c29ff0a97ff1097010a0e1913a493413baa
NAMJI <138655542+namji95@users.noreply.github.com>
2026-09-08 07:22:14 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 11.5 MB -BaekjoonHub

---
+++ Python/프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.py
@@ -0,0 +1,2 @@
+def solution(a, b, c):
+    return (a + b + c) * (a ** 2 + b ** 2 + c ** 2) * (a ** 3 + b ** 3 + c ** 3) if a == b == c else (a + b + c) * (a ** 2 + b ** 2 + c ** 2) if a == b or b == c or a == c else a + b + c
